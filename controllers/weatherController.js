5671c0f71b200267bf2761a61662a15e01d5dbf1
olgazasenko <olgazasenko@gmail.com>
2021-01-27 19:05:51 -0800

    Change the api to query by city name. Hide the API key. Impove the UI

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -1,10 +1,10 @@
 const fetch = require('node-fetch');
-const apiKey = "08739fc5060dd97e2075f6a5c0cdca51";
+require("dotenv").config();
 
 exports.getWeather = (req, res) => {
-	const cityId = req.params.city;
-	return fetch(`http://api.openweathermap.org/data/2.5/weather?id=2125072&appid=${apiKey}`)
+	const cityName = req.params.city;
+	return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${process.env.API_KEY}&units=metric`)
 		  .then(response => response.json())
 		  .then(data => res.status(200).send(data))
-		  .catch(err => console.log(`Could not fetch the weather data for city ${cityId}`, err));
+		  .catch(err => console.log(`Could not fetch the weather data for city ${cityName}`, err));
 };
--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000";
+const API_URL = "https://sweetweather.herokuapp.com";
 
 document.addEventListener('DOMContentLoaded', () => {
 	const showButton = document.querySelector('.show-weather');
@@ -25,6 +25,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	
 	
 	const showWeather = async () => {
+		clearResults();
 		const response = await fetch(`${API_URL}/v1/weather/${cityInput.value}`, {
 			method: 'GET',
 			headers: {
@@ -32,8 +33,67 @@ document.addEventListener('DOMContentLoaded', () => {
 			}
 		});
 		const data = await response.json();
-		temp.textContent = data.main.temp;
-		feelsLike.textContent = data.main.feels_like;
-		humidity.textContent = data.main.humidity;
+		if (data.cod === 200) {
+			displayWeather(data);
+		} else {
+			displayError(data);
+		}
 	};
 });
+
+function displayWeather(data) {
+	const title = document.createElement('div');
+	title.setAttribute('id', 'results-title')
+	
+	const cityName = document.createElement('h3');
+	cityName.textContent = data.name;
+	const icon = document.createElement('img');
+	icon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+	title.appendChild(cityName);
+	title.appendChild(icon);
+	
+	const children = Array(7);
+	children[0] = title;
+	children[1] = document.createElement('h4');
+	children[1].textContent = data.weather[0].main + ", " + data.weather[0].description;
+
+	let i = 2;
+	while (i < 7) {
+		children[i] = p();
+		i++;
+	}
+	children[2].textContent = "Temperature: " + data.main.temp + "\u00B0 C";
+	children[3].textContent = "Feels like: " + data.main.feels_like + "\u00B0 C";
+	children[4].textContent = "Humidity: " + data.main.humidity + "%";
+	children[5].textContent = "Pressure: " + data.main.pressure + " hPa";
+	children[6].textContent = "Wind: " + data.wind.speed + " m/sec";
+	
+	const results = getResultsElement();
+    const app = document.getElementById('root');
+	app.appendChild(results);
+	for (const el of children) {
+		results.appendChild(el);
+	}
+}
+
+function displayError(data) {
+	const errorDiv = document.createElement('div');
+	errorDiv.textContent = data.message;
+	getResultsElement().appendChild(errorDiv);
+}
+
+function clearResults() {
+	const results = getResultsElement();
+	while (results.firstChild) {
+		results.firstChild.remove();
+	}
+}
+
+function getResultsElement() {
+	return document.getElementById('results');
+}
+	
+
+const p = () => {
+	return document.createElement('p');
+};
